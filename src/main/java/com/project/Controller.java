90dad4ac3777fb429e525c9f9b75db72ba960038
PKPKrishnan <krishnan12koo@gmail.com>
2017-11-07 18:07:28 +0530

    welcome page tested

---
+++ src/main/java/com/project/Controller.java
@@ -0,0 +1,16 @@
+package com.project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @RequestMapping("/hello")
+    public ModelAndView helloWorld() {
+        System.out.println("true ---------------= " + true);
+        String message = "SPRING MVC";
+        return new ModelAndView("hellopage", "message", message);
+    }
+
+}
